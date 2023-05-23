e5006a2cc0aa6a907888f2435e435176acaa7308
nhuanh9 <chopperlouis@gmail.com>
2023-05-23 13:58:18 +0700

    done crud seat

--- src/router/seatRouter.ts
+++ src/router/seatRouter.ts
@@ -3,5 +3,8 @@ import seatController from "../controller/seatController";
 
 const seatRouter = Router()
 seatRouter.get('', seatController.all);
+seatRouter.post('', seatController.save);
+seatRouter.put('/:id', seatController.update);
+seatRouter.delete('/:id', seatController.delete);
 
 export default seatRouter
